e2e35996aceb64b5faf16944af34d80941079d1d
Pudgeros <97166337+Pudgeros@users.noreply.github.com>
2024-12-13 13:37:33 +0500

    Create IMeowable.cs

---
+++ Lab6/IMeowable.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CombinedProject
+{
+    // Интерфейс для мяукающих объектов
+    public interface IMeowable
+    {
+        void Meow();
+        void Meow(int times);
+    }
+}
